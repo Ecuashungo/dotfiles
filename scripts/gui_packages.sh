436a1c3ac815f1bae2907b9a14727bf01280cd7a
Claudio Fritsche <claudio@onesecdelivery.com>
2023-03-04 19:22:23 +0100

    Add gui_packages.sh

---
+++ scripts/gui_packages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+######################
+#### GUI Packages ####
+######################
+
+# Workstation
+sudo apt-get install -y speedcrunch
+sudo snap install slack --classic
+sudo apt install -y vlc
+
+# chromium
+sudo apt-get install -y libxssl libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
+rm -f google-chrome-stable_current_amd64.deb # clean up 
+
+
+
+# Development
+sudo snap install --classic code
+
+
+
+# Entertainment
+sudo snap install spotify
