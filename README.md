3e84c378bd7c6909792c232e4edf4066e05d6fe8
Claudio Fritsche <claudio9fritsche@hotmail.com>
2025-03-14 17:29:04 +0100

    Copy structure from shunk031

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ This repository contains the automated setup of my machines. My primary targets 
 
 # How to Set Up a new Machine
 
+```bash
+GITHUB_USERNAME="Ecuashungo"
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
+```
---
+++ install/ubuntu/common/misc.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+if [ "${DOTFILES_DEBUG:-}" ]; then
+    set -x
+fi
+
+readonly PACKAGES=(
+    #busybox
+    curl
+    cmake
+    build-essential
+    gpg
+    jq  # JSON Parser
+    htop
+    #shellcheck
+    unzip
+    vim
+    wget
+    zsh
+    plocate
+    nmap
+)
+
+function install_apt_packages() {
+    sudo apt-get install -y "${PACKAGES[@]}"
+}
+
+function uninstall_apt_packages() {
+    sudo apt-get remove -y "${PACKAGES[@]}"
+}
+
+function main() {
+    install_apt_packages
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main
+fi
