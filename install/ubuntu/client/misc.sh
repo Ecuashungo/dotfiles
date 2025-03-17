afa405ee145b91a8ddd5c8c358f405784b71d894
Claudio Fritsche <claudio9fritsche@hotmail.com>
2025-03-17 18:06:43 +0100

    bugfix: empty snap doesn't work

--- install/ubuntu/client/misc.sh
+++ install/ubuntu/client/misc.sh
@@ -17,12 +17,12 @@ readonly PACKAGES_MISC_SNAP=(
 
 function install_misc() {
     sudo apt-get install -y "${PACKAGES_MISC[@]}"
-    sudo snap install "${PACKAGES_MISC_SNAP[@]}"
+    #sudo snap install "${PACKAGES_MISC_SNAP[@]}"
 }
 
 function uninstall_misc() {
     sudo apt-get remove -y "${PACKAGES_MISC[@]}"
-    sudo snap remove "${PACKAGES_MISC_SNAP[@]}"
+    #sudo snap remove "${PACKAGES_MISC_SNAP[@]}"
 }
 
 function main() {
